5f3f47a7944a260f4abda259568da71e36b5eacd
HI-T0 <mayaimaniwahito@gmail.com>
2025-01-12 08:38:09 +0300

    first commit

---
+++ app/utils/cn.ts
@@ -0,0 +1,24 @@
+export const cn = (...classes: (string | { [key: string]: boolean })[]) => {
+
+    return classes
+  
+      .map(cls => {
+  
+        if (typeof cls === 'string') return cls;
+  
+        return Object.entries(cls)
+  
+          .filter(([, value]) => value)
+  
+          .map(([key]) => key)
+  
+          .join(' ');
+  
+      })
+  
+      .filter(Boolean)
+  
+      .join(' ');
+  
+  };
+
