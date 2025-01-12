9ce7ac29ce8dda5e57ec1eff247764d95e784ae5
HI-T0 <164635024+HI-T0@users.noreply.github.com>
2025-01-12 09:02:11 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Hito's IndieProfile,
+  title: 'My IndieProfile,
   description: 'Personal portfolio of Hito.',
 };
 
